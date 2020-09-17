417075e814e93ee76b876ec5f9b9b3879e05bf8d
rupesh <rup123@gmail.com>
2020-09-17 16:00:54 +0530

    first

---
+++ src/main/java/mgs/Call.java
@@ -0,0 +1,14 @@
+package mgs;
+
+public class Call {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=3;
+		int y=4;
+		int pro=x*y;
+		System.out.println(pro);
+
+	}
+
+}
